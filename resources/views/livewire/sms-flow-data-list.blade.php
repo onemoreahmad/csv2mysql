2af1a809c80542799019148f36a0eab06eb8ffff
Ahmad <contact@ahmad.bz>
2021-11-12 00:31:13 +0300

    colors

--- resources/views/livewire/sms-flow-data-list.blade.php
+++ resources/views/livewire/sms-flow-data-list.blade.php
@@ -2,37 +2,30 @@
     <h1 class="my-5 text-2xl font-bold leading-6">All Records <b class="text-gray-600">({{$records->total()}} records)</b></h1>
 
     @if($records->count())
-
-
-<!-- This example requires Tailwind CSS v2.0+ -->
+ 
 <div class="flex flex-col">
-    <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-      <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+    <div class="mb-3">
+        {{$records->links()}}
+    </div>
+    <div class="overflow-x-auto">
+      <div class="py-2 align-middle inline-block min-w-full">
         <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
           <table class="min-w-full divide-y divide-gray-200">
-            <thead class="bg-gray-50">
+            <thead class="bg-gray-100">
               <tr>
                 @foreach ($columns as $cl)
-                    <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                    <th scope="col" class="px-6 border-r border-gray-300 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                         {{$cl}}
                     </th>
                 @endforeach
               </tr>
             </thead>
             <tbody>
-              
-                {{-- @foreach ($records as $key => $item)
-                <tr class="bg-white">
-                    <td class="px-6 py-4 whitespace-nowrap text-sm font-medium">
-                        {{$item->DT}}
-                    </td>
-                </tr>
-                @endforeach --}}
-              
+               
                 @foreach ($records as $key => $item)
-                <tr>
+                <tr class="bg-white border-b border-gray-100 hover:bg-gray-50">
                     @foreach ($columns as $col)
-                    <td class="px-6 py-4 whitespace-nowrap text-sm font-medium">
+                    <td class="px-6 py-4 whitespace-nowrap text-sm font-medium ">
                         {{$item->$col}}
                     </td>
                     @endforeach
@@ -47,39 +40,7 @@
     </div>
   </div>
   
-
-
-
-
-
-    <table class="table w-full hidden">
-        <thead style="font-weight: bold" class="border p-3">
-            <td>DT</td>
-            <td width="100px">Remarks</td>
-            <td>ChokeSize1</td>
-            <td>US_DesanderPressurePressure</td>
-        </thead>
-        <tbody class="border p-3">
-            @foreach ($records as $item)
-            <tr>
-                <td>
-                    {{$item->DT}}
-                </td>
-                <td>
-                    {{$item->Remarks}}
-                </td>
-                <td>
-                    {{$item->ChokeSize1}}
-                </td>
-                <td>
-                    {{$item->US_DesanderPressurePressure}}
-                </td>
-            </tr>
-            @endforeach
-        </tbody>
-    </table>
-
-    <hr />
+ 
     <div class="mt-3">
         {{$records->links()}}
     </div>
--- routes/web.php
+++ routes/web.php
@@ -3,30 +3,8 @@
  Route::get('/', \App\Livewire\Home::class);
  Route::get('/', \App\Livewire\Home::class);
 
-
-// form 
-// Route::get('/form', function () {
-//     return view('form', ['records' => \App\Models\SMSFlowBack::paginate(50)]);
-// });
-
-// // process and show data .. 
-// Route::post('/data', function () {
- 
-//     request()->validate([
-//         'file' => 'required',
-//     ]);
- 
-//     Excel::import(new \App\Imports\SMSFlowBackImport, request()->file('file'));
-
-//     return redirect('/')->with('success', 'Data imported successfully.');   
-// });
-
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
 // Route::get('/dashboard', function () {
 //     return view('dashboard');
 // })->middleware(['auth'])->name('dashboard');
 
-// require __DIR__.'/auth.php';
+require __DIR__.'/auth.php';
